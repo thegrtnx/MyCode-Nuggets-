6348282299a99f030e90bb1ae68918da181f62b6
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-07-14 15:14:46 +0100

    Add files via upload

---
+++ ajaxtest.php
@@ -0,0 +1,47 @@
+<?php
+
+print_r($_POST);
+print_r($_FILES);
+?>
+
+<form id="data" method="post" enctype="multipart/form-data">
+    <input type="text" name="first" value="Bob" />
+    <input type="text" name="middle" value="James" />
+    <input type="text" name="last" value="Smith" />
+    <input name="image" type="file" />
+    <button>Submit</button>
+</form>
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-migrate-3.0.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/jquery.animateNumber.min.js"></script>
+<script src="js/jquery.waypoints.min.js"></script>
+
+<script src="js/bootstrap-datepicker.min.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/rangeslider.min.js"></script>
+<script>
+$("form#data").submit(function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+
+    $.ajax({
+        url: window.location.pathname,
+        type: 'POST',
+        data: formData,
+        success: function(data) {
+            alert(data)
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+});
+</script>
